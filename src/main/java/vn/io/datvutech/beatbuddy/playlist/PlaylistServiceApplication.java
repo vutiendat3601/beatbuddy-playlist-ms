adf242b850420ed59f5d91e0ec597ee57662253e
vutiendat3601 <vutien.dat.3601@gmail.com>
2024-03-15 14:52:46 +0700

    chore: inital commit

---
+++ src/main/java/vn/io/datvutech/beatbuddy/playlist/PlaylistServiceApplication.java
@@ -0,0 +1,11 @@
+package vn.io.datvutech.beatbuddy.playlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PlaylistServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/vn/io/datvutech/beatbuddy/playlist/PlaylistServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.io.datvutech.beatbuddy.playlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
